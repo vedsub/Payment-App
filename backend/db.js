30db51a17f02046ac688a5e8c3dd939cece925b2
Vedant Pradhan <115373199+vedsub@users.noreply.github.com>
2024-02-11 01:34:57 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const { string } = require("zod");
-dotenv.config();
 
-mongoose.connect(process.env.URI);
+console.log(process.env.MONGODB_URI);
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting:', err));
+
 
 const historySchema = new mongoose.Schema({
   name: {
