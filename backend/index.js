e1782511ac4012ad8d8856952f35823c56bb10cb
Vedant Pradhan <115373199+vedsub@users.noreply.github.com>
2024-02-11 01:35:56 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,13 @@ const express = require("express");
 const mainRouter = require("./routes/index");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT;
 
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1", mainRouter);
 
-app.listen(3000, () => {
-  console.log("App is running in PORT 3000 " );
+app.listen(PORT, () => {
+  console.log("App is running in PORT " + PORT);
 });
